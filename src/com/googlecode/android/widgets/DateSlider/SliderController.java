eafed3bea84930c96b7cdf76cb42293fa1caebb2
Chris F. Ravenscroft <chris@voilaweb.com>
2011-06-29 06:09:07 +0000

    Initial commit based on v1.2

---
+++ src/com/googlecode/android/widgets/DateSlider/SliderController.java
@@ -0,0 +1,36 @@
+package com.googlecode.android.widgets.DateSlider;
+
+import android.content.Context;
+import com.commonsware.cwac.parcel.ParcelHelper;
+
+public class SliderController {
+
+    static private SliderController instance_;
+    private Context mContext = null;
+    private ParcelHelper mParcel = null;
+
+    private SliderController(Context context) {
+        mContext = context;
+    }
+
+    static public SliderController instance(Context context) {
+        if(null == instance_) {
+            instance_ = new SliderController(context);
+        }
+        return instance_;
+    }
+
+    static public SliderController instance() {
+        if(null == instance_) {
+            throw new Error("Sorry, SliderController instance should already exist.");
+        }
+        return instance_;
+    }
+
+    public ParcelHelper getParcel() {
+        if(null == mParcel) {
+            mParcel = new ParcelHelper("dateandmoreslider", mContext);
+        }
+        return mParcel;
+    }
+}
